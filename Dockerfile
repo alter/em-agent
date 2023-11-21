f00780c2ca26c522608fc5d7022f64f2600692dc
Roman Dolgov <rdolgov@web3tech.ru>
2023-11-21 18:06:00 +0100

    try to remake

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,29 @@
-FROM python:3-alpine
-MAINTAINER Roman Dolgov 'alter.pub@gmail.com'
+# Use a specific version of the python:3-alpine image for reproducibility
+FROM python:3.12-alpine
+
+# It's good practice to avoid using personal email addresses directly in Dockerfiles
+LABEL maintainer="alter.pub@gmail.com"
+
 WORKDIR /app
-COPY requirements.txt /app
+
+# Copy only the requirements.txt initially to leverage Docker cache
+COPY requirements.txt /app/
+
+# Update apk and install dependencies in one RUN command to reduce image layers
 RUN apk update && \
-    apk --no-cache add nmap curl-dev python3-dev libressl-dev gcc musl-dev && \
-    pip install --no-cache-dir -r requirements.txt && \
-    apk del python3-dev libressl-dev gcc musl-dev && \
+    apk upgrade && \
+    apk add --no-cache nmap curl-dev python3-dev openssl-dev gcc musl-dev && \
+    pip install --no-cache-dir -r requirements.txt
+
+# After pip install, remove unnecessary packages to reduce image size
+RUN apk del python3-dev libressl-dev gcc musl-dev && \
     rm -rf /var/lib/{apt,dpkg,cache,log}/ /var/cache/apk/*
+
+# Copy the rest of your application
 COPY . /app
-CMD [ "python", "agent.py"]
+
+# Expose port 8000 for your application
 EXPOSE 8000
+
+# Set the default command for the container
+CMD ["python", "agent.py"]
--- agent.py
+++ agent.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 
-
+import os
+import yaml
+import logging
+import multiprocessing
+from datetime import datetime, timedelta
+import time
 from flask import Flask, render_template, make_response
-from time_converter import TimeConverter
-from yamlreader import yaml_load
+import config
 from port_check import PortCheck
 from web_check import WebCheck
-from queue import Queue
-from time import sleep
-import threading
-import config
-import yaml
-import os
+
+# Set up logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
 app = Flask(__name__)
 app.secret_key = os.urandom(42)
-lock = threading.Lock()
-q_web_checks = Queue()
-q_port_checks = Queue()
-webcheck_results = {}
-portcheck_results = {}
+manager = multiprocessing.Manager()
+webcheck_results = manager.dict()
+portcheck_results = manager.dict()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -36,72 +38,99 @@ def metrics():
 
 
 def merge_configs():
-    yamlconfig = yaml_load(config.CHECKS_FOLDER)
+    checks_folder = config.CHECKS_FOLDER
+    yamlconfig = {}
+    for filename in os.listdir(checks_folder):
+        if filename.endswith('.yaml'):
+            file_path = os.path.join(checks_folder, filename)
+            with open(file_path, 'r') as file:
+                file_config = yaml.safe_load(file)
+                yamlconfig.update(file_config)  # Merging the configuration
+
     with open(config.DEFAULT_CONFIG, 'wt') as out:
-        print(yaml.dump(yamlconfig, default_flow_style=False), file=out)
+        yaml.dump(yamlconfig, out, default_flow_style=False)
 
 
 def load_config():
     with open(config.DEFAULT_CONFIG, 'r') as fp:
-        yamlconfig = yaml.safe_load(fp)
-    return yamlconfig
+        return yaml.safe_load(fp)
 
 
-def make_web_check(item):
+def TimeConverter(interval):
+    if interval.endswith('s'):
+        return int(interval[:-1])
+    elif interval.endswith('m'):
+        return int(interval[:-1]) * 60
+    elif interval.endswith('h'):
+        return int(interval[:-1]) * 60 * 60
+    elif interval.endswith('d'):
+        return int(interval[:-1]) * 60 * 60 * 24
+    elif interval.endswith('w'):
+        return int(interval[:-1]) * 60 * 60 * 24 * 7
+    else:
+        return int(interval)  # Assuming default is seconds
+
+
+def worker(task_queue, result_dict):
     while True:
-        webcheck = WebCheck(item)
-        webcheck.make_request()
-        webcheck_results[webcheck.name] = webcheck.success
-        sleep(int(TimeConverter(webcheck.update_interval)))
+        item = task_queue.get()
+        if item is None:
+            break  # Exit condition
+
+        if 'url' in item:  # Assuming presence of 'url' indicates a WebCheck
+            web_check = WebCheck(item)
+            result = web_check.make_request()  # Perform the check
+            result_dict[item['name']] = result
+        elif 'port' in item:  # Assuming presence of 'port' indicates a PortCheck
+            port_check = PortCheck(item)
+            result = port_check.make_request()  # Perform the check
+            result_dict[item['name']] = result
+        else:
+            logging.error(f"Unknown check type for item: {item}")
+            continue
 
 
-def web_checks_worker():
+def start_checks(check_list, result_dict, task_queue):
+    num_cpus = multiprocessing.cpu_count()
+    num_workers = min(2 * num_cpus, len(check_list))
+
+    for _ in range(num_workers):
+        multiprocessing.Process(target=worker, args=(task_queue, result_dict)).start()
+
+
+def scheduler(task_queue, checks_config):
+    next_run_times = {}
+
     while True:
-        item = q_web_checks.get()
-        make_web_check(item)
-        q_web_checks.task_done()
+        current_time = datetime.now()
+        for check_type in ['web_checks', 'port_checks']:
+            for item in checks_config[check_type]:
+                interval = item.get('update_interval', '60')  # Use default if not provided
+                next_run_time = next_run_times.get(item['name'], current_time)
 
+                if current_time >= next_run_time:
+                    task_queue.put(item)
+                    next_run_times[item['name']] = current_time + timedelta(seconds=TimeConverter(interval))
 
-def make_web_checks(check_list):
-    num_threads = len(check_list)
-    for i in range(num_threads):
-        threading.Thread(target=web_checks_worker, daemon=True).start()
-    for x in check_list:
-        q_web_checks.put(x)
-
-
-def make_port_check(item):
-    while True:
-        portcheck = PortCheck(item)
-        portcheck.make_request()
-        portcheck_results[portcheck.name] = portcheck.success
-        sleep(int(TimeConverter(portcheck.update_interval)))
-
-
-def port_checks_worker():
-    while True:
-        item = q_port_checks.get()
-        make_port_check(item)
-        q_port_checks.task_done()
-
-
-def make_port_checks(check_list):
-    num_threads = len(check_list)
-    for i in range(num_threads):
-        threading.Thread(target=port_checks_worker, daemon=True).start()
-    for x in check_list:
-        q_port_checks.put(x)
+        time.sleep(1)  # Sleep to prevent constant CPU usage
 
 
 def setup_app(app):
     merge_configs()
     checks_config = load_config()
-    make_web_checks(checks_config['web_checks'])
-    make_port_checks(checks_config['port_checks'])
+    print(checks_config)
 
+    task_queue = multiprocessing.Queue()
 
-setup_app(app)
+    # Start worker processes
+    start_checks(checks_config['web_checks'], webcheck_results, task_queue)
+    start_checks(checks_config['port_checks'], portcheck_results, task_queue)
+
+    # Start scheduler process
+    scheduler_process = multiprocessing.Process(target=scheduler, args=(task_queue, checks_config))
+    scheduler_process.start()
 
 
 if __name__ == "__main__":
+    setup_app(app)
     app.run(host=config.HOST, port=config.PORT, threaded=True)
